94edfec64fd9daa5c088ee15aab64f7409e33d6c
dharun-08 <dharun08kumar@gmail.com>
2024-01-07 20:13:29 +0530

    first commit committ

---
+++ main.js
@@ -0,0 +1,21 @@
+const apiKey = "7640d43f04fcc3a8f231ac06e1ffb596";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind-speed").innerHTML = data.wind.speed;
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
